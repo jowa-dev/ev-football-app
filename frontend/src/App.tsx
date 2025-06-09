c5ecbc626daa72a57b7a704f8308f068d5f845b7
jowa <jowa@email.com>
2025-06-09 17:19:29 +0100

    Initial setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import WhoIsPlaying from './pages/WhoIsPlaying';
+
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<WhoIsPlaying />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+export default App
---
+++ frontend/src/pages/WhoIsPlaying.tsx
@@ -0,0 +1,16 @@
+import '../components/styles/who-is-playing.css';
+
+const WhoIsPlaying = () => {
+  return (
+    <div className="main-container">
+      <div className="header">
+        <h1>Who is playing?</h1>
+      </div>
+      <div className="form-container">
+
+      </div>
+    </div>
+  )
+};
+
+export default WhoIsPlaying;
