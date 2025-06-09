44acc021a90cb080ecf32f2b4d99d2648f41548d
jowa <jowa@email.com>
2025-06-09 17:34:44 +0100

    install formik + radix

--- frontend/src/pages/WhoIsPlaying.tsx
+++ frontend/src/pages/WhoIsPlaying.tsx
@@ -1,13 +1,27 @@
 import '../components/styles/who-is-playing.css';
+import { Formik, Field, Form } from 'formik';
+import { Button } from '@radix-ui/themes';
 
 const WhoIsPlaying = () => {
   return (
     <div className="main-container">
       <div className="header">
         <h1>Who is playing?</h1>
+        <Button variant="surface" color="purple">
+          Hello
+        </Button>
       </div>
       <div className="form-container">
+        <Formik
+          initialValues={{ playerName: '' }}
+          onSubmit={(values) => {
+            console.log('Submitted values:', values);
+          }}
+        >
+          <Form>
 
+          </Form>
+        </Formik>
       </div>
     </div>
   )
