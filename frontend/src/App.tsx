84cfae26af2345799117e1fc7320f44e63dea264
w4rrz <161767121+w4rrz@users.noreply.github.com>
2025-06-12 00:02:46 +0100

    Updates people updates

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
+import { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import WhoIsPlaying from './pages/WhoIsPlaying';
 
-const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<WhoIsPlaying />} />
-    </Routes>
-  </BrowserRouter>
-);
+const App = () => {
+  const [players, setPlayers] = useState([]);
+  console.log(players);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<WhoIsPlaying setPlayers={setPlayers} />} />
+      </Routes>
+    </BrowserRouter>
+  )
+
+}
 
 export default App;
--- frontend/src/pages/WhoIsPlaying.tsx
+++ frontend/src/pages/WhoIsPlaying.tsx
@@ -1,62 +1,70 @@
-import { useState } from 'react';
 import '../components/styles/who-is-playing.css';
-import { Formik, Field, Form } from 'formik';
-import { Button, Card, Flex, Checkbox, Box, ScrollArea } from '@radix-ui/themes';
+import { Formik, Form } from 'formik';
+import { Button, Card, Flex, Checkbox, Box, ScrollArea, Text } from '@radix-ui/themes';
 import playerData from '../../players.json';
+import { useState } from 'react';
 
-const chunkArray = (arr: any[], size: number) => {
-  const chunks = [];
-  for (let i = 0; i < arr.length; i += size) {
-    chunks.push(arr.slice(i, i + size));
-  }
-  return chunks;
-};
+const WhoIsPlaying = ({ setPlayers }: { setPlayers: any }) => {
+  const [playerAmount, setPlayerAmount] = useState(0);
 
-const WhoIsPlaying = () => {
-  console.log(playerData);
-  const [cardsStart, setCardsStart] = useState(0);
-  const [cardsEnd, setCardsEnd] = useState(4);
-  const slicedPlayers = playerData.players.slice(cardsStart, cardsEnd);
-  const splitPlayers = chunkArray(playerData.players, 4);
+  const mappedPlayerData = playerData.players.map((player) => ({
+    ...player,
+    isPlaying: false
+  }));
+
+  const handleSubmit = (values: any) => {
+    const players = values.players.filter(({ isPlaying }: { isPlaying: boolean }) => isPlaying);
+    setPlayers(players);
+  };
+
   return (
     <div className="main-container">
       <div className="header">
         <h1>Who is playing?</h1>
       </div>
       <Formik
-        initialValues={playerData.players}
-        onSubmit={(values) => {
-          console.log('Submitted values:', values);
-        }}
+        initialValues={{ players: mappedPlayerData }}
+        onSubmit={(values) => handleSubmit(values)}
       >
-        <Form>
-          <Card>
-            <ScrollArea type="always" scrollbars="vertical" style={{ height: 400, width: 300 }}>
-              <Box pr="3">
-                {playerData?.players.map(({ name, rating }) => (
-                  <Card key={name} style={{ margin: 10 }}>
-                    <Field>
-                      {({ field }: any) => (
+        {({ values, setFieldValue }) => (
+          <Form style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+            <Box width={{ initial: '85%', xs: '80%', sm: '60%', md: '50%', lg: '30%', xl: '30%' }}>
+              <Card>
+                <ScrollArea type="always" scrollbars="vertical" style={{ height: 450 }}>
+                  <Box pr="3">
+                    {values.players.map((player, idx) => (
+                      <Card key={player.name} style={{ margin: 10 }}>
                         <Flex gap="10px" align="center" justify="between">
-                          {name}
+                          <Flex direction="column">
+                            <Text size="2" color="plum" weight="medium">{player.name} ({player.rating})</Text>
+                            <Text size="2" color="gray" weight="medium">{player.position}</Text>
+                          </Flex>
                           <Checkbox
-                            defaultChecked
+                            checked={player.isPlaying}
                             color="plum"
                             variant="surface"
-                            size="2"
+                            name={`players[${idx}].isPlaying`}
+                            size="3"
+                            onCheckedChange={(checked) => {
+                              setFieldValue(`players[${idx}].isPlaying`, checked)
+                              setPlayerAmount((prev) => checked ? prev + 1 : prev - 1)
+                            }}
                           />
                         </Flex>
-                      )}
-                    </Field>
-                  </Card>
-                ))}
-              </Box>
-            </ScrollArea>
-            <Button variant="surface" color="plum" type="submit" style={{ marginTop: 10 }}>
-              Next
-            </Button>
-          </Card>
-        </Form>
+                      </Card>
+                    ))}
+                  </Box>
+                </ScrollArea>
+                <Box style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginTop: 12 }}>
+                  <Text size="2" color="pink" weight="medium">Selected Players: {playerAmount}</Text>
+                  <Button variant="surface" color="plum" type="submit">
+                    Next
+                  </Button>
+                </Box>
+              </Card>
+            </Box>
+          </Form>
+        )}
       </Formik>
     </div>
   )
